35b44089e0b1fe37a40d863a75a4d1aefe7efc51
digitalby <digitalby@users.noreply.github.com>
2019-10-11 00:31:43 +0300

    Work In Progress: Logs now display key names

--- LangesterTutorial3/Source.c
+++ LangesterTutorial3/Source.c
@@ -1,4 +1,189 @@
-#include "ntddk.h" //Typically used in windows kernel driver development
+//ntddk.h is typically used in windows kernel driver development
+#include "ntddk.h" 
+
+PCHAR keyNamesQWERTYStandard[] = {
+    "0x00", //0x00
+    "ESC", //0x01
+    "1", //0x02
+    "2", //0x03
+    "3", //0x04
+    "4", //0x05
+    "5", //0x06
+    "6", //0x07
+    "7", //0x08
+    "8", //0x09
+    "9", //0x0a
+    "0", //0x0b
+    "MINUS", //0x0c
+    "EQUAL", //0x0d
+    "BACKSPACE", //0x0e
+    "TAB", //0x0f
+    "Q", //0x10
+    "W", //0x11
+    "E", //0x12
+    "R", //0x13
+    "T", //0x14
+    "Y", //0x15
+    "U", //0x16
+    "I", //0x17
+    "O", //0x18
+    "P", //0x19
+    "LEFT_BRACKET", //0x1a
+    "RIGHT_BRACKET", //0x1b
+    "RETURN", //0x1c
+    "CTRL", //0x1d
+    "A", //0x1e
+    "S", //0x1f
+    "D", //0x20
+    "F", //0x21
+    "G", //0x22
+    "H", //0x23
+    "J", //0x24
+    "K", //0x25
+    "L", //0x26
+    "SEMICOLON", //0x27
+    "APOSTROPHE", //0x28
+    "GRAVE", //0x29
+    "LEFT_SHIFT", //0x2a
+    "0x2b", //0x2b
+    "Z", //0x2c
+    "X", //0x2d
+    "C", //0x2e
+    "V", //0x2f
+    "B", //0x30
+    "N", //0x31
+    "M", //0x32
+    "COMMA", //0x33
+    "PERIOD", //0x34
+    "SLASH", //0x35
+    "RIGHT_SHIFT", //0x36
+    "KP_ASTERISK", //0x37
+    "ALT", //0x38
+    "SPACEBAR", //0x39
+    "CAPSLOCK", //0x3a
+    "F1", //0x3b
+    "F2", //0x3c
+    "F3", //0x3d
+    "F4", //0x3e
+    "F5", //0x3f
+    "F6", //0x40
+    "F7", //0x41
+    "F8", //0x42
+    "F9", //0x43
+    "F10", //0x44
+    "NUM_LOCK", //0x45
+    "SCROLL_LOCK", //0x46
+    "KP_7", //0x47
+    "KP_8", //0x48
+    "KP_9", //0x49
+    "KP_MINUS", //0x4a
+    "KP_4", //0x4b
+    "KP_5", //0x4c
+    "KP_6", //0x4d
+    "KP_PLUS", //0x4e
+    "KP_1", //0x4f
+    "KP_2", //0x50
+    "KP_3", //0x51
+    "KP_0", //0x52
+    "KP_PERIOD", //0x53
+    "0x54", //0x54
+    "0x55", //0x55
+    "ISO_KEY", //0x56
+    "F11", //0x57
+    "F12" //0x58
+};
+
+PCHAR keyNamesColemakStandard[] = {
+    "0x00", //0x00
+    "ESC", //0x01
+    "1", //0x02
+    "2", //0x03
+    "3", //0x04
+    "4", //0x05
+    "5", //0x06
+    "6", //0x07
+    "7", //0x08
+    "8", //0x09
+    "9", //0x0a
+    "0", //0x0b
+    "MINUS", //0x0c
+    "EQUAL", //0x0d
+    "BACKSPACE", //0x0e
+    "TAB", //0x0f
+    "Q", //0x10
+    "W", //0x11
+    "F", //0x12
+    "P", //0x13
+    "G", //0x14
+    "J", //0x15
+    "L", //0x16
+    "U", //0x17
+    "Y", //0x18
+    "SEMICOLON", //0x19
+    "LEFT_BRACKET", //0x1a
+    "RIGHT_BRACKET", //0x1b
+    "RETURN", //0x1c
+    "CTRL", //0x1d
+    "A", //0x1e
+    "R", //0x1f
+    "S", //0x20
+    "T", //0x21
+    "D", //0x22
+    "H", //0x23
+    "N", //0x24
+    "E", //0x25
+    "I", //0x26
+    "O", //0x27
+    "APOSTROPHE", //0x28
+    "GRAVE", //0x29
+    "LEFT_SHIFT", //0x2a
+    "US_BACKSLASH", //0x2b
+    "Z", //0x2c
+    "X", //0x2d
+    "C", //0x2e
+    "V", //0x2f
+    "B", //0x30
+    "K", //0x31
+    "M", //0x32
+    "COMMA", //0x33
+    "PERIOD", //0x34
+    "SLASH", //0x35
+    "RIGHT_SHIFT", //0x36
+    "KP_ASTERISK", //0x37
+    "ALT", //0x38
+    "SPACEBAR", //0x39
+    "CAPSLOCK", //0x3a
+    "F1", //0x3b
+    "F2", //0x3c
+    "F3", //0x3d
+    "F4", //0x3e
+    "F5", //0x3f
+    "F6", //0x40
+    "F7", //0x41
+    "F8", //0x42
+    "F9", //0x43
+    "F10", //0x44
+    "NUM_LOCK", //0x45
+    "SCROLL_LOCK", //0x46
+    "KP_7", //0x47
+    "KP_8", //0x48
+    "KP_9", //0x49
+    "KP_MINUS", //0x4a
+    "KP_4", //0x4b
+    "KP_5", //0x4c
+    "KP_6", //0x4d
+    "KP_PLUS", //0x4e
+    "KP_1", //0x4f
+    "KP_2", //0x50
+    "KP_3", //0x51
+    "KP_0", //0x52
+    "KP_PERIOD", //0x53
+    "0x54", //0x54
+    "0x55", //0x55
+    "ISO_KEY", //0x56
+    "F11", //0x57
+    "F12" //0x58
+};
 
 typedef struct {
     PDEVICE_OBJECT LowerKbdDevice;
@@ -79,7 +264,7 @@ NTSTATUS WillAttachDevice(PDRIVER_OBJECT DriverObject)
 //Called when the READ request's completion routine is to be executed.
 NTSTATUS ReadWillComplete(PDEVICE_OBJECT DeviceObject, PIRP Irp, PVOID Context)
 {
-    CHAR* keyFlagValues[4] = { "KEY_MAKE", "KEY_BREAK", "EXTENDED0", "EXTENDED1" };
+    PCHAR keyFlagValues[4] = { "KEY_MAKE", "KEY_BREAK", "EXTENDED0", "EXTENDED1" };
     //Get the keyboard's key input buffer.
     PKEYBOARD_INPUT_DATA keyBuffer = (PKEYBOARD_INPUT_DATA)Irp->AssociatedIrp.SystemBuffer;
     //Get the number of bytes being transferred in the buffer, then convert it to the number of items in array.
@@ -90,7 +275,17 @@ NTSTATUS ReadWillComplete(PDEVICE_OBJECT DeviceObject, PIRP Irp, PVOID Context)
         //...loop through each buffered keypress being read...
         for (int i = 0; i < numberOfItemsInBuffer; i++) {
             //...and log the acquired scan code and its flag value (usually either KEY_MAKE or KEY_BREAK).
-            DbgPrint("ReadWillComplete got scan code %x (%s)\r\n", keyBuffer->MakeCode, keyFlagValues[keyBuffer->Flags]);
+            if (keyBuffer->Flags < 2)
+                //If KEY_MAKE or KEY_BREAK flag is present, also log the key name.
+                DbgPrint("ReadWillComplete got scan code %x/%s (QWERTY: %s; Colemak:%s)\r\n",
+                    keyBuffer->MakeCode,
+                    keyFlagValues[keyBuffer->Flags],
+                    keyNamesQWERTYStandard[keyBuffer->MakeCode],
+                    keyNamesColemakStandard[keyBuffer->MakeCode]
+                    );
+            else DbgPrint("ReadWillComplete got scan code %x/%s\r\n",
+                keyBuffer->MakeCode,
+                keyFlagValues[keyBuffer->Flags]);
         }
     }
 
